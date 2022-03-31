8243a909ffbbc0ac376e1c7515ff2948bbce8348
MateusBAlmeida <99480555+MateusBAlmeida@users.noreply.github.com>
2022-03-31 00:41:35 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,8 @@ async function bootstrap() {
     }),
   );
   
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
 }
 bootstrap();
