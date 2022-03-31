cb21376fa6e0c125863aafaca5e4d174e134782d
MateusBAlmeida <99480555+MateusBAlmeida@users.noreply.github.com>
2022-03-31 20:55:40 -0300

    Implementação de Token JWT

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,6 +8,7 @@ import {
   } from '@nestjs/common';
   import { AuthService } from './auth.service';
   import { LocalAuthGuard } from './guards/local-auth.guard';
+import { AuthRequest } from './models/auth-request';
   
   
   @Controller()
@@ -18,7 +19,9 @@ import {
     @UseGuards(LocalAuthGuard)
     @Post('login')
     @HttpCode(HttpStatus.OK)
-    async login() {
-    //   return this.authService.login(req.user);
+    async login(@Request() req: AuthRequest) {
+        console.log(req.user);
+        
+        return this.authService.login(req.user);
     }
   }
