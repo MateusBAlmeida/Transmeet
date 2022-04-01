006dd74b9df7e46ae46f02728bd075d32d309c56
MateusBAlmeida <99480555+MateusBAlmeida@users.noreply.github.com>
2022-03-31 21:30:52 -0300

    Correção IsPublic

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -7,6 +7,7 @@ import {
   import { Reflector } from '@nestjs/core';
   // Password
   import { AuthGuard } from '@nestjs/passport';
+
   // Decorators
   import { IS_PUBLIC_KEY } from '../decorators/is-public.decorator';
   // Error Handling
