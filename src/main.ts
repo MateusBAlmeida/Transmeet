083634a9c041d415e3eb9849e45811f8459e6e1e
MateusBAlmeida <99480555+MateusBAlmeida@users.noreply.github.com>
2022-04-13 16:00:24 -0300

    Permissão de Header

--- src/main.ts
+++ src/main.ts
@@ -27,7 +27,7 @@ async function bootstrap() {
   app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type');
     res.setHeader('Access-Control-Allow-Credentials', true);
     // handle OPTIONS method
     if ('OPTIONS' == req.method) {
